1fe87082b4fb2f7eef6e9c1ea939ce64fc71ce49
Alexander <pchp250g8@tutamail.com>
2025-12-25 12:04:01 +0300

    Добавьте файлы проекта.

---
+++ RunningString/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RunningString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.CursorVisible = false;
+            var strSpaces = new String(' ', 80);
+            var strLine = "Hello World!!! Running String!!! Press Any Key To Exit!!!";
+            strLine = strSpaces + strLine + strSpaces;
+            var nCounter = 0;
+            var nStrLen = strLine.Length - 80;
+            do
+            {
+                strSpaces = strLine.Substring(nCounter, 80);
+                Console.SetCursorPosition(10, 10);
+                Console.Write(strSpaces);
+                Thread.Sleep(140);
+                nCounter++;
+                if (nCounter >= nStrLen) nCounter = 0;
+            } while (!Console.KeyAvailable);
+        }
+    }
+}
